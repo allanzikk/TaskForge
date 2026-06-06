f7aba1e277b2e229dacd2188782ec6fa4c09a627
allanzikk <allanjo12321@gmail.com>
2026-06-05 23:35:20 -0300

    tirando prefixo api

--- api/__init__.py
+++ api/__init__.py
@@ -5,7 +5,7 @@ from .projects.routes import projects_bp
 from .tasks.routes import tasks_bp
 from .users.routes import users_bp
 
-api_bp = Blueprint("api", __name__, url_prefix="/api")
+api_bp = Blueprint("api", __name__)
 
 api_bp.register_blueprint(auth_bp)
 api_bp.register_blueprint(org_bp)
