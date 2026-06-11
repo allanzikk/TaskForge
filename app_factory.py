899eb99a0efcd4776b408cc8c6a4dc657f2bbf68
allanzikk <allanjo12321@gmail.com>
2026-06-10 21:27:27 -0300

    corrigindo uploads v4

--- app_factory.py
+++ app_factory.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 from api import api_bp
 from api.organizations.events import *
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +13,8 @@ def create_app():
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-secret-key")
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY", "dev-jwt-key")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
     db.init_app(app)
     bcrypt.init_app(app)
